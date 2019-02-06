dd3e48d82028d0c5b57227558796a127fe9f91da
idaskovbjerg <ida_skovbjerg@hotmail.com>
2019-02-06 13:45:13 +0100

    Changes in the console app: Useage of the accumulator is now possible.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,19 +6,71 @@ namespace Calculator //https://codereview.stackexchange.com/questions/126141/fir
     class Program
     {
         
-        private static readonly string[] operations = { "+", "-", "*", "^"};
+        private static readonly string[] operations = { "+", "-", "*", "/", "^"};
+        public static CalculatorClass calc_ { get; set; } = new CalculatorClass();
         static void Main(string[] args)
         {
+           
             
-            Calculator();
-            
-            Console.WriteLine("New calculation? [Y]/[N]");
+            Console.WriteLine("Use accumulator? [Y]/[N]");
+            while (Console.ReadLine() == "Y")
+            {
+                CalculatorAccumulator();
+                Console.WriteLine("Continue to use accumulator? [Y]/[N]");
+
+            }
+            Console.WriteLine("Do you wanna use the  normal calculator? [Y]/[N]");
             while (Console.ReadLine() == "Y")
             {
                 Calculator();
                 Console.WriteLine("New calculation? [Y]/[N]");
             }
-            
+        }
+
+        
+        static void CalculatorAccumulator()
+        {
+
+            double result = 0.0;
+            string stringOperation =
+                SetOperation(
+                    "Enter the operation: + (addition), - (subtraction), * (multiplication), / (division) or ^ (power):");
+
+            double firstNumber;
+
+            switch (stringOperation)
+            {
+                case "+":
+                case "addition":
+                    firstNumber = SetNumber("First number : ");
+                    //result = calc.Addition(firstNumber);
+                    Console.WriteLine("Result: {0} {1} {2} = {3}", calc_.Accumulator, stringOperation, firstNumber, calc_.Addition(firstNumber));
+                    break;
+                case "-":
+                case "subtraction":
+                    firstNumber = SetNumber("First number : ");
+                    
+                    Console.WriteLine("Result: {0} {1} {2} = {3}", calc_.Accumulator, stringOperation, firstNumber, calc_.Subtraction(firstNumber));
+                    break;
+                case "*":
+                case "multiplication":
+                    firstNumber = SetNumber("First number : ");
+                    
+                    Console.WriteLine("Result: {0} {1} {2} = {3}", calc_.Accumulator, stringOperation, firstNumber, calc_.Multiplication(firstNumber));
+                    break;
+                case "/":
+                case "division":
+                    firstNumber = SetNumber("First number : ");
+
+                    Console.WriteLine("Result: {0} {1} {2} = {3}", calc_.Accumulator, stringOperation, firstNumber, calc_.Division(firstNumber));
+                    break;
+                case "^":
+                case "power":
+                    firstNumber = SetNumber("First number : ");
+                    
+                    Console.WriteLine("Result: {0}{1}{2} = {3}", calc_.Accumulator, stringOperation, firstNumber, calc_.Power(firstNumber));
+                    break;
+            }
         }
 
         static void Calculator()
@@ -28,7 +80,7 @@ namespace Calculator //https://codereview.stackexchange.com/questions/126141/fir
             double result = 0.0;
             string stringOperation =
                 SetOperation(
-                    "Enter the operation: + (addition), - (subtraction), * (multiplication) or ^ (power):");
+                    "Enter the operation: + (addition), - (subtraction), * (multiplication), / (division) or ^ (power):");
 
             double firstNumber;
             double secondNumber;
@@ -54,6 +106,13 @@ namespace Calculator //https://codereview.stackexchange.com/questions/126141/fir
                     firstNumber = SetNumber("First number : ");
                     secondNumber = SetNumber("Second number: ");
                     result = calc.Multiplication(firstNumber, secondNumber);
+                    Console.WriteLine("Result: {0} {1} {2} = {3}", firstNumber, stringOperation, secondNumber, result);
+                    break;
+                case "/":
+                case "division":
+                    firstNumber = SetNumber("First number : ");
+                    secondNumber = SetNumber("Second number: ");
+                    result = calc.Division(firstNumber, secondNumber);
                     Console.WriteLine("Result: {0} {1} {2} = {3}", firstNumber, stringOperation, secondNumber, result);
                     break;
                 case "^":
