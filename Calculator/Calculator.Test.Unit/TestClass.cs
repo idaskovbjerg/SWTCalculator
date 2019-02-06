9d7fb8bc6540bab20a5099517da0482ce48ab3b1
idaskovbjerg <ida_skovbjerg@hotmail.com>
2019-02-06 13:08:20 +0100

    More test done and approved

--- Calculator/Calculator.Test.Unit/TestClass.cs
+++ Calculator/Calculator.Test.Unit/TestClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
 
 
 namespace Calculator.Test.Unit
@@ -8,47 +10,67 @@ namespace Calculator.Test.Unit
     [TestFixture]
     public class TestClass
     {
-        private readonly Calculator.CalculatorClass calculator;
+        private Calculator.CalculatorClass calculator;
 
-        public TestClass()
+        [SetUp]
+        public void SetupTest()
         {
             calculator = new CalculatorClass();
         }
 
         [Test]
-        public void AdditionTest()
+        public void Addition_PositiveNumber_ResultIsCorrect()
         {
             var x = calculator.Addition(2, 2);
             Assert.That(x,Is.EqualTo(4));
         }
         [Test]
-        public void AdditionTest2()
+        public void Addition_NegativeNumberFirst_ResultIsCorrect()
         {
             var x = calculator.Addition(-30, 27);
             Assert.That(x, Is.EqualTo(-30+27));
         }
+        [Test]
+        public void Addition_NegativeNumberLast_ResultIsCorrect()
+        {
+            var x = calculator.Addition(30, -27);
+            Assert.That(x, Is.EqualTo(30 + (-27)));
+        }
+        
 
         [Test]
-        public void SubtractionTest()
+        public void Subtraction_PositiveNumber_ResultIsCorrect()
         {
             var x = calculator.Subtraction(29, 37);
             Assert.That(x,Is.EqualTo(-8));
         }
         [Test]
-        public void SubtractionTest2()
+        public void Subtraction_NegativeNumberLast_ResultIsCorrect()
         {
-            var x = calculator.Subtraction(2936, 37);
-            Assert.That(x, Is.EqualTo(2936-37));
+            var x = calculator.Subtraction(2936, -37);
+            Assert.That(x, Is.EqualTo(2936-(-37)));
+        }
+        [Test]
+        public void Subtraction_NegativeNumberFirst_ResultIsCorrect()
+        {
+            var x = calculator.Subtraction(-2936, 37);
+            Assert.That(x, Is.EqualTo(-2936 - 37));
         }
 
         [Test]
-        public void MultiplicationTest()
+        public void Multiplication_NegativeNumberLast_ResultIsCorrect()
         {
             var x = calculator.Multiplication(3.7, -3.3);
             Assert.That(x,Is.EqualTo(3.7*-3.3));
         }
         [Test]
-        public void MultiplicationTest2()
+        public void Multiplication_NegativeNumberFirst_ResultIsCorrect()
+        {
+            var x = calculator.Multiplication(-3.7, 3.3);
+            Assert.That(x, Is.EqualTo(-3.7 * 3.3));
+        }
+        [Test]
+        public void Multiplication_PositiveNumber_ResultIsCorrect()
         {
             var x = calculator.Multiplication(37, 3.3);
             Assert.That(x, Is.EqualTo(37 * 3.3));
@@ -60,37 +82,156 @@ namespace Calculator.Test.Unit
         [TestCase(3, -2, -6)]
         [TestCase(0, -2, 0)]
         [TestCase(-2, 0, 0)]
+        [TestCase(-2.2, 2, -4.4)]
         [TestCase(0, 0, 0)]
-        public void Multiply_MultiplyNunmbers_ResultIsCorrect(int a, int b, int result)
+        public void Multiplication_MultiplyNumbers_ResultIsCorrect(double a, double b, double result)
         {
             Assert.That(calculator.Multiplication(a, b), Is.EqualTo(result));
         }
 
+        [TestCase(8, 2, 4)]
+        [TestCase(8, -2, -4)]
+        [TestCase(-16, -2, 8)]
+        [TestCase(-1.6, -2, 0.8)]
+        [TestCase(0, 16, 0)]
+        public void Division_DivideNumbers_ResultIsCorrect(double a, double b, double result)
+        {
+            Assert.That(calculator.Division(a,b), Is.EqualTo(result));
+        }
+
         [Test]
-        public void PowerTest()
+        public void Divide_DivideByZero_ThrowException()
+        {
+            Assert.Throws<ArgumentException>(() => calculator.Division(16, 0));
+
+            // Assert.That(() => calculator.Division(16,0), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Power_PositiveDecimalNumbers_ResultIsCorrect()
         {
             var x = calculator.Power(37, 3.3);
             Assert.That(x, Is.EqualTo(149646.2).Within(0.01));
         }
 
         [Test]
-        public void PowerTest2()
+        public void Power_NegativeDecimalNumberLast_ResultIsCorrect()
         {
             var x = calculator.Power(37, -3.3);
             Assert.That(x, Is.EqualTo(Math.Pow(37,-3.3)).Within(0.01));
         }
         [Test]
-        public void PowerTest3()
+        public void Power_NegativeDecimalNumbers_ResultIsCorrect()
         {
             var x = calculator.Power(-37, -3);
             Assert.That(x, Is.EqualTo(Math.Pow(-37,-3)).Within(0.01));
         }
 
+        // Only one parameter
         [TestCase(2, 2)]
-        [TestCase(2, 2)]
-        public void Addition_ResultIsCorrect(double a, double result)
+        [TestCase(-2, -2)]
+        [TestCase(2.2, 2.2)]
+        public void Addition_AdditionNumbers_ResultIsCorrect(double a, double result)
         {
             Assert.That(calculator.Addition(a), Is.EqualTo(result));
         }
+
+        [TestCase(2, 4)]
+        [TestCase(-2, 0)]
+        [TestCase(2.2, 4.2)]
+        public void Addition_AdditionNumber_AccEqualsTwo_ResultIsCorrect(double a, double result)
+        {
+            calculator.Addition(2);
+            Assert.That(calculator.Addition(a), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Clear_ClearAccumulator()
+        {
+            calculator.Clear();
+            Assert.AreEqual(calculator.Accumulator, 0);
+        }
+
+        [TestCase(2, -2)]
+        [TestCase(-2, 2)]
+        [TestCase(-6.6, 6.6)]
+        public void Subtraction_SubtractionNumbers_ResultIsCorrect(double a, double result)
+        {
+            Assert.That(calculator.Subtraction(a), Is.EqualTo(result));
+        }
+
+        [TestCase(2, 0)]
+        [TestCase(-2, 4)]
+        [TestCase(-6.6, 8.6)]
+        public void Subtraction_SubtractionNumbers_AccEqualsTwo_ResultIsCorrect(double a, double result)
+        {
+            calculator.Addition(2);
+            Assert.That(calculator.Subtraction(a), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 0)]
+        [TestCase(-2, 0)]
+        [TestCase(2.2, 0)]
+        public void Multiplication_MultiplicationNumbers_ResultIsCorrect(double a, double result)
+        {
+            Assert.That(calculator.Multiplication(a), Is.EqualTo(result).Within(0.001));
+        }
+        [TestCase(2, 4)]
+        [TestCase(-2, -4)]
+        [TestCase(2.2, 4.4)]
+        public void Multiplication_MultiplicationNumbers_AccEqualsTwo_ResultIsCorrect(double a, double result)
+        {
+            calculator.Addition(2);
+            Assert.That(calculator.Multiplication(a), Is.EqualTo(result).Within(0.001));
+        }
+
+        [TestCase(2, 0)]
+        [TestCase(-3, 0)]
+        [TestCase(5.5, 0)]
+        public void Division_DivideNumber_ResultIsCorrect(double a, double result)
+        {
+            Assert.That(calculator.Division(a), Is.EqualTo(result).Within(0.001));
+        }
+
+        [TestCase(2, 1)]
+        [TestCase(-3, -0.667)]
+        [TestCase(5.5, 0.3636)]
+        public void Division_DivideNumber_AccEqualsTwo_ResultIsCorrect(double a, double result)
+        {
+            calculator.Addition(2); // We start by adding by two. 
+            Assert.That(calculator.Division(a), Is.EqualTo(result).Within(0.001));
+        }
+
+        [Test]
+        public void Division_DivideByZero_ThrowException()
+        {
+            Assert.That(() => calculator.Division(0), Throws.TypeOf<DivideByZeroException>());
+        }
+
+        [TestCase(2, 0)]
+        [TestCase(0.1, 0)]
+        [TestCase(0, 1)]
+        public void Power_PowerNumbers_ResultIsCorrect(double a, double result)
+        {
+            Assert.That(calculator.Power(a), Is.EqualTo(result).Within(0.001));
+        }
+
+        [Test]
+        public void Power_NegativeExponent_AccEqualsZero_ThrowException()
+        {
+            Assert.That(() => calculator.Power(-5), Throws.TypeOf<ArgumentException>());
+        }
+
+
+        [TestCase(2, 4)]
+        [TestCase(-5, 0.03125)]
+        [TestCase(0.1, 1.0718)]
+        public void Power_PowerNumbers_AccEqualsTwo_ResultIsCorrect(double a, double result)
+        {
+            calculator.Addition(2);
+            Assert.That(calculator.Power(a), Is.EqualTo(result).Within(0.001));
+        }
+
     }
 }
--- Calculator/Calculator/CalculatorClass.cs
+++ Calculator/Calculator/CalculatorClass.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace Calculator
 {
@@ -26,8 +27,15 @@ namespace Calculator
 
         public double Division(double a, double b)
         {
-            result = a / b;
-            return result;
+
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new ArgumentException("Cannot divide by zero");
+            }
         }
 
         public double Power(double x, double exp)
@@ -56,14 +64,28 @@ namespace Calculator
 
         public double Division(double divisor)
         {
-            Accumulator /= divisor;
-            return Accumulator;
+            if (divisor != 0)
+            {
+                Accumulator /= divisor;
+                return Accumulator;
+            }
+            else
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
         }
 
         public double Power(double exponent)
         {
-            Accumulator = Math.Pow(Accumulator, exponent);
-            return Accumulator;
+            if (exponent >= 0 || Accumulator != 0)
+            {
+                Accumulator = Math.Pow(Accumulator, exponent);
+                return Accumulator;
+            }
+            else
+            {
+                throw new ArgumentException("Cannot use negative exponent while Accumulator is zero");
+            }
         }
 
 
